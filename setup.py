1d55a243df4d69a63613a0db8bb03103284c026e
Tom Most <twm@freecog.net>
2015-05-27 18:01:05 -0700

    Add Trove classifiers

--- setup.py
+++ setup.py
@@ -16,6 +16,13 @@ setup(
             'gir=gir:curses_main',
         ],
     },
+    classifiers=[
+        'Environment :: Console :: Curses',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Version Control',
+    ],
     packages=find_packages(),
     include_package_data=False,
     zip_safe=True,
